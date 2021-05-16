fed6d45e5aa0475291fbffdf3c45f572fe10eb0c
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2021-05-16 18:10:39 -0500

    ok

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -7,7 +7,7 @@ const path = require("path");
 module.exports = {
   entry: {
     index: "./src/index.js",
-    
+    script: "./src/script.js"
   },
   output: {
     filename: "bundler.[contenthash].js",
