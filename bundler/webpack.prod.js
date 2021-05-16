57d2b4e6484c35b7000a28f014e575703e5b43ec
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2021-05-16 12:48:25 -0500

    still working on displaying images for production

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -3,7 +3,14 @@ const commonConfiguration = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = merge(commonConfiguration, {
-  
+  performance: {
+    maxAssetSize: 500000000,
+    maxEntrypointSize: 500000000,
+    hints: "error",
+    assetFilter: function (assetFilename) {
+      return !assetFilename.endsWith(".jpg, .mp4, .png, .glb");
+    },
+  },
   mode: "production",
   devtool: "source-map",
   plugins: [new CleanWebpackPlugin()],
