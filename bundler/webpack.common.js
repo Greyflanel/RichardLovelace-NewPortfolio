f1658793c38b58f8e88751aff6f40ce7dd43575a
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2021-05-24 18:02:06 -0500

    pulled dist folder out of bundler folder

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: ["./src/index.js", "./src/script.js"],
   output: {
     filename: "bundler.[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "../dist"),
     clean: true,
     
   },
